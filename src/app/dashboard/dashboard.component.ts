8b0abee1dc7679a1bad422bdcadeb4be11c6cd97
Abhijit Haldar <abhijithaldar08@gmail.com>
2025-05-09 02:57:15 +0530

    ngFor & Nested ngFor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,6 +16,12 @@ export class DashboardComponent implements OnInit {
   CurrentExpenditure?: number;
   AvailableFunds?: number;
 
+  Clients: string[] = [];
+  Projects: string[] = [];
+  Years: number[] = [];
+  TeamMembersSummary: { Region: string; TeamMembersCount: number; TemporarilyUnavailableMembers: number; }[] = [];
+  TeamMembers: { Region: string, Members: { ID: number, Name: string, Status: string }[] }[] = [];
+
   ngOnInit() {
     this.Designation = "Project Manager";
     this.Username = "John Doe";
@@ -26,5 +32,75 @@ export class DashboardComponent implements OnInit {
     this.ProjectCost = 50000;
     this.CurrentExpenditure = 20000;
     this.AvailableFunds = 30000;
+
+    this.Clients = ["ABC Infotech Ltd", "DEF Software Solutions", "GHI Industries"];
+
+    this.Projects = ["Project A", "Project B", "Project C", "Project D"];
+
+    for (let i = 2010; i <= 2025; i++) {
+      this.Years.push(i);
+    }
+
+    this.TeamMembersSummary = [
+      {
+        Region: "East",
+        TeamMembersCount: 20,
+        TemporarilyUnavailableMembers: 4
+      },
+      {
+        Region: "West",
+        TeamMembersCount: 15,
+        TemporarilyUnavailableMembers: 2
+      },
+      {
+        Region: "South",
+        TeamMembersCount: 12,
+        TemporarilyUnavailableMembers: 3
+      },
+      {
+        Region: "North",
+        TeamMembersCount: 10,
+        TemporarilyUnavailableMembers: 1
+      }
+    ]
+
+    this.TeamMembers = [
+      {
+        Region: "East",
+        Members: [
+          { ID: 1, Name: "Ford", Status: "Available" },
+          { ID: 2, Name: "Miller", Status: "Available" },
+          { ID: 3, Name: "Jones", Status: "Busy" },
+          { ID: 4, Name: "James", Status: "Busy" },
+        ]
+      },
+      {
+        Region: "West",
+        Members: [
+          { ID: 5, Name: "Smith", Status: "Available" },
+          { ID: 6, Name: "Johnson", Status: "Available" },
+          { ID: 7, Name: "Brown", Status: "Busy" },
+          { ID: 8, Name: "Davis", Status: "Busy" },
+        ]
+      },
+      {
+        Region: "South",
+        Members: [
+          { ID: 9, Name: "Wilson", Status: "Available" },
+          { ID: 10, Name: "Taylor", Status: "Available" },
+          { ID: 11, Name: "Anderson", Status: "Busy" },
+          { ID: 12, Name: "Thomas", Status: "Busy" },
+        ]
+      },
+      {
+        Region: "North",
+        Members: [
+          { ID: 13, Name: "Jackson", Status: "Available" },
+          { ID: 14, Name: "White", Status: "Available" },
+          { ID: 15, Name: "Harris", Status: "Busy" },
+          { ID: 16, Name: "Martin", Status: "Busy" },
+        ]
+      }
+    ]
   }
 }

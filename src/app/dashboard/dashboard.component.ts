05e1ec53ffd0db5c5dc3a12ca67de606292ff46f
Abhijit Haldar <abhijithaldar08@gmail.com>
2025-05-09 01:08:52 +0530

    Create Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  Designation?: string;
+  Username?: string;
+  NoOfTeamMembers?: number;
+  TotalCostOfAllProjects?: number;
+  PendingTasks?: number;
+  UpComingProjects?: number;
+  ProjectCost?: number;
+  CurrentExpenditure?: number;
+  AvailableFunds?: number;
 
+  ngOnInit() {
+    this.Designation = "Project Manager";
+    this.Username = "John Doe";
+    this.NoOfTeamMembers = 5;
+    this.TotalCostOfAllProjects = 100000;
+    this.PendingTasks = 3;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 50000;
+    this.CurrentExpenditure = 20000;
+    this.AvailableFunds = 30000;
+  }
 }

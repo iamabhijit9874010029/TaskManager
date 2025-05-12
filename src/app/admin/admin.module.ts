92ed24c69a8dbb427a2f47482e1550f8dd941d0a
Abhijit Haldar <abhijithaldar08@gmail.com>
2025-05-13 01:34:10 +0530

    Modules

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    AboutComponent,
+    MyProfileComponent
+  ],
+  imports: [
+    CommonModule, //AppModule does not require CommonModule because it already imports BrowserModule, which includes all the features of CommonModule (and more) Only import BrowserModule once, in the root AppModule. All feature modules (like AdminModule, UserModule, etc.) should import CommonModule, not BrowserModule.
+  ],
+  exports: [
+    DashboardComponent,
+    AboutComponent,
+    MyProfileComponent
+  ]
+})
+export class AdminModule { }

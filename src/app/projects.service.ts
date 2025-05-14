5eea6f063cb8168f61c969b95ee0401debbe30ed
Abhijit Haldar <abhijithaldar08@gmail.com>
2025-05-14 22:35:11 +0530

    Get [Asp.Net Core MVC]3

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -11,6 +11,7 @@ export class ProjectsService {
   constructor(private httpClient: HttpClient) { }
 
   getAllProjects(): Observable<Project[]> {
-    return this.httpClient.get<Project[]>('https://localhost:7062/api/projects');
+    // return this.httpClient.get<Project[]>('/api/projects'); //with proxy forwarding (need to create proxy.conf.json and add to angular.json, package.json)
+    return this.httpClient.get<Project[]>('https://localhost:7062/api/projects'); //with cross-origin
   }
 }

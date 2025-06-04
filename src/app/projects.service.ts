196cf408374051c98c864583afbe0c158c4e1c7c
Abhijit Haldar <abhijithaldar08@gmail.com>
2025-06-04 22:19:19 +0530

    JSON-Server API calls (for Non Asp.Net Core Developers)

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -12,30 +12,43 @@ export class ProjectsService {
 
   getAllProjects(): Observable<Project[]> {
     // return this.httpClient.get<Project[]>('/api/projects'); //with proxy forwarding (need to create proxy.conf.json and add to angular.json, package.json)
-    return this.httpClient.get<Project[]>('https://localhost:7062/api/projects', { responseType: 'json' }).pipe(
+    // return this.httpClient.get<Project[]>('https://localhost:7062/api/projects', { responseType: 'json' }).pipe(
+    //   map((data: Project[]) => {
+    //     for (let i = 0; i < data.length; i++) {
+    //       data[i].teamSize = Math.round(data[i].teamSize! * 100);
+    //     }
+    //     return data;
+    //   })
+    // ); //with cross-origin
+
+    return this.httpClient.get<Project[]>('http://localhost:3000/projects', { responseType: 'json' }).pipe(
       map((data: Project[]) => {
         for (let i = 0; i < data.length; i++) {
           data[i].teamSize = Math.round(data[i].teamSize! * 100);
         }
         return data;
       })
-    ); //with cross-origin
+    ); //for json-server //with cross-origin
   }
 
   insertProject(newProject: Project): Observable<Project> {
-    return this.httpClient.post<Project>('https://localhost:7062/api/projects', newProject, { responseType: 'json' });
+    // return this.httpClient.post<Project>('https://localhost:7062/api/projects', newProject, { responseType: 'json' });
+    return this.httpClient.post<Project>('http://localhost:3000/projects', newProject, { responseType: 'json' });//for json-server 
   }
 
   updateProject(existingProject: Project): Observable<Project> {
-    return this.httpClient.put<Project>('https://localhost:7062/api/projects', existingProject, { responseType: 'json' });
+    return this.httpClient.put<Project>('http://localhost:3000/projects', existingProject, { responseType: 'json' });//for json-server 
   }
 
   deleteProject(projectID: number): Observable<number> {
     // return this.httpClient.delete<number>(`https://localhost:7062/api/projects/${projectID}`); //ProjectID as route parameter (path segment) in backend API
-    return this.httpClient.delete<number>('https://localhost:7062/api/projects?ProjectID=' + projectID); //ProjectID as a query parameter in backend API
+    // return this.httpClient.delete<number>('https://localhost:7062/api/projects?ProjectID=' + projectID); //ProjectID as a query parameter in backend API
+    return this.httpClient.delete<number>('http://localhost:3000/projects?ProjectID=' + projectID);//for json-server  //ProjectID as a query parameter in backend API
   }
 
   searchProjects(searchBy: string, searchText: string): Observable<Project[]> {
-    return this.httpClient.get<Project[]>(`https://localhost:7062/api/projects/${searchBy}/${searchText}`, { responseType: 'json' });
+    // return this.httpClient.get<Project[]>(`https://localhost:7062/api/projects/${searchBy}/${searchText}`, { responseType: 'json' });
+    return this.httpClient.get<Project[]>(`http://localhost:3000/projects/${searchBy}/${searchText}`, { responseType: 'json' });//for json-server (this will not work with json-server, as it does not support dynamic routes like this)
+    // Instead, you can filter the projects on the client side after fetching all projects
   }
 }
